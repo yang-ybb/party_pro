6a01051251647273f5408802fb99cb9a785b9288
vagrant <vagrant@localhost.localdomain>
2015-12-06 07:31:58 -0500

    code domain and persistence layer without test.

---
+++ src/org/party/domain/Commit.java
@@ -0,0 +1,130 @@
+package org.party.domain;
+
+public class Commit {
+    public Integer getUid() {
+		return uid;
+	}
+	public void setUid(Integer uid) {
+		this.uid = uid;
+	}
+	public Integer getRudangshenqingshu() {
+		return rudangshenqingshu;
+	}
+	public void setRudangshenqingshu(Integer rudangshenqingshu) {
+		this.rudangshenqingshu = rudangshenqingshu;
+	}
+	public Integer getJijifenzikaochabiao() {
+		return jijifenzikaochabiao;
+	}
+	public void setJijifenzikaochabiao(Integer jijifenzikaochabiao) {
+		this.jijifenzikaochabiao = jijifenzikaochabiao;
+	}
+	public Integer getZizhuan() {
+		return zizhuan;
+	}
+	public void setZizhuan(Integer zizhuan) {
+		this.zizhuan = zizhuan;
+	}
+	public Integer getSixianghuibao() {
+		return sixianghuibao;
+	}
+	public void setSixianghuibao(Integer sixianghuibao) {
+		this.sixianghuibao = sixianghuibao;
+	}
+	public Integer getZhengshengcailiao() {
+		return zhengshengcailiao;
+	}
+	public void setZhengshengcailiao(Integer zhengshengcailiao) {
+		this.zhengshengcailiao = zhengshengcailiao;
+	}
+	public Integer getZhengshenbaogao() {
+		return zhengshenbaogao;
+	}
+	public void setZhengshenbaogao(Integer zhengshenbaogao) {
+		this.zhengshenbaogao = zhengshenbaogao;
+	}
+	public Integer getQunzhongyijian() {
+		return qunzhongyijian;
+	}
+	public void setQunzhongyijian(Integer qunzhongyijian) {
+		this.qunzhongyijian = qunzhongyijian;
+	}
+	public Integer getDangxiaojieyezheng() {
+		return dangxiaojieyezheng;
+	}
+	public void setDangxiaojieyezheng(Integer dangxiaojieyezheng) {
+		this.dangxiaojieyezheng = dangxiaojieyezheng;
+	}
+	public Integer getTuiyourudang() {
+		return tuiyourudang;
+	}
+	public void setTuiyourudang(Integer tuiyourudang) {
+		this.tuiyourudang = tuiyourudang;
+	}
+	public Integer getZhibudahuijilu() {
+		return zhibudahuijilu;
+	}
+	public void setZhibudahuijilu(Integer zhibudahuijilu) {
+		this.zhibudahuijilu = zhibudahuijilu;
+	}
+	public Integer getFazhandangyuangongshi() {
+		return fazhandangyuangongshi;
+	}
+	public void setFazhandangyuangongshi(Integer fazhandangyuangongshi) {
+		this.fazhandangyuangongshi = fazhandangyuangongshi;
+	}
+	public Integer getRudangzhiyuanshu() {
+		return rudangzhiyuanshu;
+	}
+	public void setRudangzhiyuanshu(Integer rudangzhiyuanshu) {
+		this.rudangzhiyuanshu = rudangzhiyuanshu;
+	}
+	public Integer getBanqizongjie() {
+		return banqizongjie;
+	}
+	public void setBanqizongjie(Integer banqizongjie) {
+		this.banqizongjie = banqizongjie;
+	}
+	public Integer getQuannianzongjie() {
+		return quannianzongjie;
+	}
+	public void setQuannianzongjie(Integer quannianzongjie) {
+		this.quannianzongjie = quannianzongjie;
+	}
+	public Integer getZhuanzhengshenqing() {
+		return zhuanzhengshenqing;
+	}
+	public void setZhuanzhengshenqing(Integer zhuanzhengshenqing) {
+		this.zhuanzhengshenqing = zhuanzhengshenqing;
+	}
+	public Integer getYubeidangyuankaochabiao() {
+		return yubeidangyuankaochabiao;
+	}
+	public void setYubeidangyuankaochabiao(Integer yubeidangyuankaochabiao) {
+		this.yubeidangyuankaochabiao = yubeidangyuankaochabiao;
+	}
+	public Integer getZhuanzhenggongshi() {
+		return zhuanzhenggongshi;
+	}
+	public void setZhuanzhenggongshi(Integer zhuanzhenggongshi) {
+		this.zhuanzhenggongshi = zhuanzhenggongshi;
+	}
+	private Integer uid;
+    private Integer rudangshenqingshu;
+    private Integer jijifenzikaochabiao;
+    private Integer zizhuan;
+    private Integer sixianghuibao;
+    private Integer zhengshengcailiao;
+    private Integer zhengshenbaogao;
+    private Integer qunzhongyijian;
+    private Integer dangxiaojieyezheng;
+    private Integer tuiyourudang;
+    private Integer zhibudahuijilu;
+    private Integer fazhandangyuangongshi;
+    private Integer rudangzhiyuanshu;
+    private Integer banqizongjie;
+    private Integer quannianzongjie;
+    private Integer zhuanzhengshenqing;
+    private Integer yubeidangyuankaochabiao;
+    private Integer zhuanzhenggongshi;
+}
---
+++ src/org/party/domain/PairObjObj.java
@@ -0,0 +1,18 @@
+package org.party.domain;
+
+public class PairObjObj {
+	private Object first;
+	private Object second;
+	public Object getFirst() {
+		return first;
+	}
+	public void setFirst(Object first) {
+		this.first = first;
+	}
+	public Object getSecond() {
+		return second;
+	}
+	public void setSecond(Object second) {
+		this.second = second;
+	}
+}
